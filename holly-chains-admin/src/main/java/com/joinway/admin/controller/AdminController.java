4ca7165970aba030bc6bc1f00ad602a2e16bece1
scorpiopapa <liliang2005@gmail.com>
2014-08-16 17:45:05 +0800

    init

---
+++ holly-chains-admin/src/main/java/com/joinway/admin/controller/AdminController.java
@@ -0,0 +1,161 @@
+package com.joinway.admin.controller;
+
+import javax.validation.Valid;
+
+import org.jsondoc.core.annotation.ApiBodyObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.joinway.admin.bean.UserContext;
+import com.joinway.admin.bean.form.LoginForm;
+import com.joinway.admin.bean.form.RegisterForm;
+import com.joinway.admin.bean.view.LoginView;
+import com.joinway.admin.bean.view.LogoutView;
+import com.joinway.admin.bean.view.TreeMenuView;
+import com.joinway.admin.service.AdminService;
+import com.joinway.admin.utils.SessionHelper;
+import com.joinway.bean.exception.ValidationException;
+import com.joinway.bean.logging.annotation.InputLog;
+import com.joinway.bean.logging.annotation.OutputLog;
+import com.joinway.web.audit.ExceptionController;
+import com.joinway.web.audit.annotation.Audit;
+import com.joinway.web.security.annotation.Login;
+import com.joinway.web.security.annotation.Logout;
+import com.joinway.web.security.annotation.SecurityCheck;
+
+/**
+ * @author Administrator
+ *
+ */
+@Controller
+@RequestMapping("")
+@Validated
+public class AdminController extends ExceptionController {
+
+	private final static Logger log = LoggerFactory.getLogger(AdminController.class);
+	
+	@Autowired AdminService service;
+	
+//	@RequestMapping(value={"index"})
+//	@ResponseBody
+//	@InputLog
+//	public ModelAndView index() throws Exception {
+//		return new ModelAndView("index");
+//	}
+
+	@RequestMapping(value="register", method=RequestMethod.POST)
+	@ResponseBody
+	@Login
+	@Audit
+	@InputLog
+	@OutputLog
+	public LoginView register(@ApiBodyObject @Valid RegisterForm form) throws Exception {
+		preLogin(form.getName());
+		
+		LoginView view = service.register(form);
+		
+		postLogin(view);
+		
+		return view;
+	}
+	
+	@RequestMapping(value="login", method=RequestMethod.POST, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@Login
+	@Audit
+	@InputLog
+	@OutputLog
+	public LoginView login(@ApiBodyObject @Valid LoginForm form) throws Exception {
+		preLogin(form.getName());
+		
+		LoginView view = service.login(form);
+		
+		postLogin(view);
+		
+		return view;
+	}
+	
+	@RequestMapping(value="login/context", method=RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@SecurityCheck
+	@Audit
+	@InputLog
+	@OutputLog
+	public LoginView getLoginContext() throws ValidationException {
+		LoginView view = new LoginView();
+		
+		UserContext uc = SessionHelper.getUserContext(true);
+		view.setUserId(uc.getUserId());
+		view.setLoginName(uc.getLoginName());
+		view.setLastLoginTime(uc.getLastLoginTime());
+		view.setLoginCount(uc.getLoginCount());
+		
+		return view;
+	}
+
+	@RequestMapping(value="logout", produces=MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@Audit
+	@Logout
+	@InputLog
+	@OutputLog
+	public LogoutView logout(){
+		LogoutView view = service.logout();
+		
+		UserContext uc = SessionHelper.getUserContext();
+		if(uc != null){
+			view.setUserName(uc.getLoginName());
+		}
+		
+		log.info("user logged out");
+		
+		return view;
+	}
+	
+	@RequestMapping(value="navigator", produces=MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	@SecurityCheck
+	@Audit
+	@InputLog
+	@OutputLog
+	public TreeMenuView navigator() throws Exception {
+		return service.getNavigatorMenus(SessionHelper.getUserContext().getUserId());
+	}
+	
+	void preLogin(String userName){
+		UserContext uc = SessionHelper.getUserContext(true);
+		// set login user name anyway for audit usage
+		uc.setLoginName(userName);
+	}
+
+	void postLogin(LoginView view){
+		UserContext uc = SessionHelper.getUserContext();
+
+		uc.setLastLoginTime(view.getLastLoginTime());
+		uc.setLoginCount(view.getLoginCount());
+		uc.setUserId(view.getUserId());
+		
+//		// ..sso
+//		AppContext.set(SecurityConstants.SSO.UID_KEY, uc.getUserId());
+	}
+	
+//	@RequestMapping(value="search/{table}", produces=MediaType.APPLICATION_JSON_VALUE)
+//	@ResponseBody
+//	@Audit("search")
+//	@InputLog
+//	@OutputLog
+//	public DataGridView find(@PathVariable("table") String table, @LogIgnore HttpServletRequest request) throws Exception {
+//		driveTraineeTableServiec.
+//		return 
+//	}
+}
+
+
---
+++ holly-chains-admin/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<title>登录</title>
+</head>
+<body>
+<form id="login_form" method="post" action="index.jspx">
+	用户名<input id="name" name="name" type="text"/><br/>
+	密码<input id="password" name="password" type="password"/><br/>
+	<input type="submit" value="确定">
+</form>
+</body>
+</html>
